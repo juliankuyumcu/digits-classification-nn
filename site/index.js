a38c51c3111e0a34cea08a7415665e1fdbe86339
Julian Kuyumcu <julesjk23@hotmail.com>
2024-08-29 23:50:54 -0400

    Fixed model link for GitHub Pages

--- site/index.js
+++ site/index.js
@@ -1,6 +1,6 @@
 let model = null;
 const loadModel = async () => {
-    model = await tf.loadLayersModel('model.json');
+    model = await tf.loadLayersModel('./site/model.json');
 }
 
 loadModel();
